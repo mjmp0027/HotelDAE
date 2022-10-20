056ec6ef0d81ed0e039bd2306ec94f0941509b67
Carlos Garvín <cgr00064@red.ujaen.es>
2022-10-20 16:06:37 +0200

    Inicio ServicioHotelTest

--- src/test/java/es/ujaen/dae/hotel/servicios/ServicioHotelTest.java
+++ src/test/java/es/ujaen/dae/hotel/servicios/ServicioHotelTest.java
@@ -1,4 +1,60 @@
 package es.ujaen.dae.hotel.servicios;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.List;
+import java.util.Optional;
+import javax.validation.ConstraintViolationException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
+
+
+
 public class ServicioHotelTest {
+
+    @Autowired
+    ServicioHotel servicioHotel;
+
+    @Test
+    public void testAccesoServicioHotel(){
+        Assertions.assertThat(servicioHotel).isNotNull();
+    }
+
+    @Test
+    @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
+    public void testAltaClienteInvalido(){
+        // Cliente con e-mail incorrecto!!!
+        String clave = "manuel82";
+        Direccion direccion = new Direccion(
+                "España",
+                "Jaen",
+                "SanJuan",
+                19);
+        Cliente cliente = new Cliente(
+                3,
+                "12345678Q",
+                "Manuel Jesus",
+                "mjmp0027",
+                clave,
+                direccion,
+                123456789,
+                "mjmp0027.es"
+        );
+
+        Assertions.assertThatThrownBy(()->{
+            servicioHotel.altaCliente(cliente);})
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+
 }
