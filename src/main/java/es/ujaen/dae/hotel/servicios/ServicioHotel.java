73e62ce346a349289ccf36a5364f799dc54b1e07
manuel <mjmp0027@red.ujaen.es>
2022-11-04 17:50:33 +0100

    ServicioHotel casi adaptado a JPA

--- src/main/java/es/ujaen/dae/hotel/servicios/ServicioHotel.java
+++ src/main/java/es/ujaen/dae/hotel/servicios/ServicioHotel.java
@@ -5,6 +5,7 @@ import es.ujaen.dae.hotel.excepciones.ClienteNoRegistrado;
 import es.ujaen.dae.hotel.excepciones.ClienteYaRegistrado;
 import es.ujaen.dae.hotel.excepciones.HotelYaExiste;
 import es.ujaen.dae.hotel.excepciones.ReservaNoDisponible;
+import es.ujaen.dae.hotel.repositorios.RepositorioAdministrador;
 import es.ujaen.dae.hotel.repositorios.RepositorioCliente;
 import es.ujaen.dae.hotel.repositorios.RepositorioHotel;
 import lombok.extern.slf4j.Slf4j;
@@ -23,40 +24,34 @@ import java.util.*;
 @Slf4j
 @Validated
 public class ServicioHotel {
-    //Map<Integer, Cliente> clientes;
     @Autowired
     RepositorioCliente repositorioCliente;
-    //Map<Integer, Hotel> hoteles;
     @Autowired
     RepositorioHotel repositorioHotel;
-
-    //Mapa de administradores para la comprobacion en altaHotel
-    Map<String, Administrador> administradores;
+    @Autowired
+    RepositorioAdministrador repositorioAdministrador;
     private int numClientes;
     private int numHoteles;
 
     @PostConstruct
     private void init() {
-        administradores = new TreeMap<>();
         numClientes = 0;
         numHoteles = 0;
         //Solo vamos a ser tres administradores
         Administrador manuel = new Administrador("mjmp", "clave1");
         Administrador carlos = new Administrador("cgr", "clave2");
         Administrador maria = new Administrador("mhm", "clave3");
-        administradores.put(manuel.getUserName(), manuel);
-        administradores.put(carlos.getUserName(), carlos);
-        administradores.put(maria.getUserName(), maria);
+        repositorioAdministrador.guardar(manuel);
+        repositorioAdministrador.guardar(carlos);
+        repositorioAdministrador.guardar(maria);
     }
 
     public Cliente altaCliente(@NotNull @Valid Cliente cliente) throws ClienteNoRegistrado {
         log.info("Cliente con datos: " + cliente + " registrandose");
-        //if (clientes.containsKey(cliente.getId())) {
         if (repositorioCliente.buscar(cliente.getDni()).isPresent()) {
             throw new ClienteYaRegistrado();
         } else {
             cliente.setId(numClientes++);
-            //clientes.put(cliente.getId(), cliente);
             repositorioCliente.guardar(cliente);
             log.info("Cliente con datos: " + cliente + " registrado");
             return cliente;
@@ -64,34 +59,26 @@ public class ServicioHotel {
     }
 
     public Hotel altaHotel(@NotNull @Valid Hotel hotel, @Valid @NotNull Administrador administrador) throws Exception {
-        for (Map.Entry<String, Administrador> administradores : administradores.entrySet()) {
-            if (administradores.getValue().getUserName().equals(administrador.getUserName())
-                    && administradores.getValue().getContraseña().equals(administrador.getContraseña())) {
-                log.info("Hotel con datos: " + hotel + " registrandose");
-                //if (hoteles.containsKey(hotel.getId())) {
-                if(repositorioHotel.buscar(hotel.getId()).isPresent()){
-                    throw new HotelYaExiste();
-                } else {
-                    hotel.setId(numHoteles++);
-                    //hoteles.put(hotel.getId(), hotel);
-                    repositorioHotel.guardar(hotel);
-                    log.info("Hotel con datos: " + hotel + " registrado");
-                    return hotel;
-                }
+        if (repositorioAdministrador.buscar(administrador.getUserName()).isPresent()) {
+            log.info("Hotel con datos: " + hotel + " registrandose");
+            if (repositorioHotel.buscar(hotel.getId()).isPresent()) {
+                throw new HotelYaExiste();
+            } else {
+                hotel.setId(numHoteles++);
+                repositorioHotel.guardar(hotel);
+                log.info("Hotel con datos: " + hotel + " registrado");
+                return hotel;
             }
         }
         throw new Exception("Administrador no valido");
     }
 
-    //TODO
     @Transactional
     public Optional<Cliente> loginCliente(@NotNull String userName, @NotNull String clave) {
-        Optional<Cliente> cliente = Optional.empty();
-        for (Map.Entry<Integer, Cliente> clientes : clientes.entrySet()) {
-            if (clientes.getValue().getUserName().equals(userName) && clientes.getValue().claveValida(clave))
-                cliente = Optional.of(clientes.getValue());
-        }
-        return cliente;
+        Optional<Cliente> clienteLogin = repositorioCliente.buscar(userName)
+                .filter((cliente) -> cliente.claveValida(clave));
+
+        return clienteLogin;
 //        return Optional
 //                .ofNullable(clientes.get(userName))
 //                .filter((cliente)
@@ -99,39 +86,39 @@ public class ServicioHotel {
     }
 
     //TODO
-    public List<Hotel> buscarHoteles(Direccion direccion, LocalDateTime fechaIni, LocalDateTime fechaFin) {
+    public List<Hotel> buscarHoteles(Direccion direccion, LocalDateTime fechaIni, LocalDateTime fechaFin, int numDoble, int numSimple) {
         List<Hotel> listaHoteles = new ArrayList<>();
-        for (Map.Entry<Integer, Hotel> hoteles : hoteles.entrySet()) {
-            for (int i = 0; i < hoteles.getValue().getReservasActuales().size(); i++) {
-                if (hoteles.getValue().getDireccion() == direccion) {
-                    if (fechaIni.isBefore(hoteles.getValue().getReservasActuales().get(i).getFechaInicio())
-                            && fechaFin.isBefore(hoteles.getValue().getReservasActuales().get(i).getFechaInicio())
-                            || fechaIni.isAfter(hoteles.getValue().getReservasActuales().get(i).getFechaFin())) {
-                        listaHoteles.add(hoteles.getValue());
-                    } else {
-                        throw new ReservaNoDisponible();
-                    }
-                } else {
-                    throw new ReservaNoDisponible();
-                }
-            }
-        }
+//        for (Map.Entry<Integer, Hotel> hoteles : hoteles.entrySet()) {
+//            for (int i = 0; i < hoteles.getValue().getReservasActuales().size(); i++) {
+//                if (hoteles.getValue().getDireccion() == direccion) {
+//                    if (fechaIni.isBefore(hoteles.getValue().getReservasActuales().get(i).getFechaInicio())
+//                            && fechaFin.isBefore(hoteles.getValue().getReservasActuales().get(i).getFechaInicio())
+//                            || fechaIni.isAfter(hoteles.getValue().getReservasActuales().get(i).getFechaFin())
+//                            && hoteles.getValue().getNumDobl() > numDoble && hoteles.getValue().getNumSimp() > numSimple) {
+//                        listaHoteles.add(hoteles.getValue());
+//                    } else {
+//                        throw new ReservaNoDisponible();
+//                    }
+//                } else {
+//                    throw new ReservaNoDisponible();
+//                }
+//            }
+//        }
         return listaHoteles;
     }
-    
-    boolean hacerReserva(@NotNull @Valid Cliente cliente, Direccion direccion, LocalDateTime fechaIni, LocalDateTime fechaFin, int numDoble, int numSimple) {
 
-        List<Hotel> listaHoteles = buscarHoteles(direccion, fechaIni, fechaFin);
-        //Suponemos que se queda con el primer hotel que hay en esa direccion
-        //if(clientes.containsKey(cliente.getId()))
-        if(repositorioCliente.buscar(cliente.getDni()).isPresent())
-        if (listaHoteles.get(0).getNumDobl() >= numDoble && listaHoteles.get(0).getNumSimp() >= numSimple) {
-            Reserva reserva = new Reserva(direccion, fechaIni, fechaFin, numSimple, numDoble);
-            cliente.addReserva(reserva);
-            listaHoteles.get(0).setNumSimp(numSimple);
-            listaHoteles.get(0).setNumDobl(numDoble);
-            return true;
-        }
+    boolean hacerReserva(@NotNull @Valid Cliente cliente, Direccion direccion, LocalDateTime fechaIni, LocalDateTime fechaFin, int numDoble, int numSimple, Hotel hotel) {
+
+        if (buscarHoteles(direccion, fechaIni, fechaFin, numDoble, numSimple).contains(hotel))
+
+            if (repositorioCliente.buscar(cliente.getDni()).isPresent())
+                if (hotel.getNumDobl() >= numDoble && hotel.getNumSimp() >= numSimple) {
+                    Reserva reserva = new Reserva(direccion, fechaIni, fechaFin, numSimple, numDoble);
+                    repositorioCliente.nuevaReserva(cliente, reserva);
+                    hotel.setNumSimp(numSimple);
+                    hotel.setNumDobl(numDoble);
+                    return true;
+                }
         return false;
     }
 }
