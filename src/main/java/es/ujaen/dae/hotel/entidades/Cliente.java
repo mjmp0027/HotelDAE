4cb7067dfffb4fe1952267d36763e4e3e34ace36
Carlos Garvín <cgr00064@red.ujaen.es>
2022-11-15 18:06:18 +0100

    Arreglado el Test testAltayLoginCliente

--- src/main/java/es/ujaen/dae/hotel/entidades/Cliente.java
+++ src/main/java/es/ujaen/dae/hotel/entidades/Cliente.java
@@ -2,6 +2,7 @@ package es.ujaen.dae.hotel.entidades;
 
 import es.ujaen.dae.hotel.utils.ExprReg;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Embedded;
@@ -11,34 +12,44 @@ import javax.validation.constraints.*;
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Cliente implements Serializable {
 
     @NotBlank
     @Size(min=9, max=9)
     @Pattern(regexp = ExprReg.DNI)
-    private final String dni;
+    private String dni;
 
     @NotBlank
-    private final String nombre;
+    private String nombre;
 
     @Id
     @NotBlank
-    private final String userName;
+    private String userName;
 
     @NotBlank
-    private final String contraseña;
+    private String contraseña;
 
     @NotNull
     @Embedded
-    private final Direccion direccion;
+    private Direccion direccion;
 
     @Pattern(regexp = ExprReg.TLF)
-    private final String tlf;
+    private String tlf;
 
     @Email
-    private final String email;
+    private String email;
+
+    public Cliente(String dni, String nombre, String userName, String contraseña, Direccion direccion, String tlf, String email){
+        this.dni = dni;
+        this.nombre = nombre;
+        this.userName = userName;
+        this.contraseña = contraseña;
+        this.direccion = direccion;
+        this.tlf = tlf;
+        this.email = email;
+    }
 
     public boolean claveValida(String clave) {
         return contraseña.equals(clave);
