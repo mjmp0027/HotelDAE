627f75678f0473e632ee40e85c6e46e5124286db
Carlos Garvín <cgr00064@red.ujaen.es>
2022-12-19 10:29:37 +0100

    Iniciado primer test

--- src/test/java/es/ujaen/dae/hotel/rest/ControladorRESTTest.java
+++ src/test/java/es/ujaen/dae/hotel/rest/ControladorRESTTest.java
@@ -1,15 +1,26 @@
 package es.ujaen.dae.hotel.rest;
 
 
+import es.ujaen.dae.hotel.entidades.Administrador;
+import es.ujaen.dae.hotel.entidades.Cliente;
+import es.ujaen.dae.hotel.entidades.Direccion;
+import es.ujaen.dae.hotel.entidades.Hotel;
+import es.ujaen.dae.hotel.excepciones.AdministradorYaExiste;
+import es.ujaen.dae.hotel.rest.dto.DTOCliente;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.annotation.PostConstruct;
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
 @SpringBootTest(classes = es.ujaen.dae.hotel.HotelDaeApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,10 +38,38 @@ public class ControladorRESTTest {
     @PostConstruct
     void crearRestTemplateBuilder() {
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder()
-                .rootUri("http://localhost:" + localPort + "/ujacoin")
+                .rootUri("http://localhost:" + localPort + "/hoteldae")
                 .additionalMessageConverters(List.of(springBootJacksonConverter));
 
         restTemplate = new TestRestTemplate(restTemplateBuilder);
     }
 
+    @Test
+    public void testAltaClienteInvalido() {
+        // Cliente con e-mail incorrecto!!!
+        String clave = "manuel82";
+        Direccion direccion = new Direccion(
+
+                "España",
+                "Jaen",
+                "SanJuan",
+                19);
+
+        //Registramos cliente
+        DTOCliente cliente = new DTOCliente(
+                "12345678Q",
+                "Manuel Jesus",
+                "mjmp0027",
+                clave,
+                direccion,
+                "657550655",
+                "mjmp0027gmail.com"
+        );
+        ResponseEntity<DTOCliente> respuesta = restTemplate.postForEntity(
+                "/clientes",
+                cliente,
+                DTOCliente.class
+        );
+        Assertions.assertThat(respuesta.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
 }
